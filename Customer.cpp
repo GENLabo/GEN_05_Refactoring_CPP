2b56641c9a9eb66189b1792ad9161897f2447afa
Robin <r.demarta@outlook.com>
2020-06-11 11:10:53 +0200

    Refactored for each in statement()

--- Customer.cpp
+++ Customer.cpp
@@ -14,35 +14,33 @@ string Customer::statement()
 {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
-    vector< Rental >::iterator iter = _rentals.begin();
-    vector< Rental >::iterator iter_end = _rentals.end();
     ostringstream result;
     result << "Rental Record for " << getName() << "\n";
-    for ( ; iter != iter_end; ++iter ) {
+    
+    for (Rental rental : _rentals) {
         double thisAmount = 0;
-        Rental each = *iter;
 
         // determine amounts for each line
-        switch ( each.getMovie().getPriceCode() ) {
+        switch ( rental.getMovie().getPriceCode() ) {
             case Movie::REGULAR:
                 thisAmount += 2;
-                if ( each.getDaysRented() > 2 )
-                    thisAmount += ( each.getDaysRented() - 2 ) * 1.5 ;
+                if ( rental.getDaysRented() > 2 )
+                    thisAmount += ( rental.getDaysRented() - 2 ) * 1.5 ;
                 break;
             case Movie::NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie::CHILDRENS:
                 thisAmount += 1.5;
-                if ( each.getDaysRented() > 3 )
-                    thisAmount += ( each.getDaysRented() - 3 ) * 1.5;
+                if ( rental.getDaysRented() > 3 )
+                    thisAmount += ( rental.getDaysRented() - 3 ) * 1.5;
                 break;
         }
 
-        frequentRenterPoints = each.updateFrequentRenterPoint(frequentRenterPoints);
+        frequentRenterPoints = rental.updateFrequentRenterPoint(frequentRenterPoints);
 
         // show figures for this rental
-        result << "\t" << each.getMovie().getTitle() << "\t"
+        result << "\t" << rental.getMovie().getTitle() << "\t"
                << thisAmount << "\n";
         totalAmount += thisAmount;
     }
