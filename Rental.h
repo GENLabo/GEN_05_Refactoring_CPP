62d5baad3829f9b9576dd670a8252bbec9ed6198
Robin <r.demarta@outlook.com>
2020-06-11 11:08:25 +0200

    Externalize frequent points calculations into Rental

--- Rental.h
+++ Rental.h
@@ -10,6 +10,17 @@ public:
     int getDaysRented() const;
     const Movie& getMovie() const;
 
+    int updateFrequentRenterPoint(int currentPoints) {
+        // add frequent renter points
+        ++currentPoints;
+
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie::NEW_RELEASE)
+             && getDaysRented() > 1 ) currentPoints++;
+
+        return currentPoints;
+    }
+
 private:
     Movie _movie;
     int _daysRented;
