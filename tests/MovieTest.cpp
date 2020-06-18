3eb97c4eea30d47298776a559a2cd5e407503342
Robin <r.demarta@outlook.com>
2020-06-18 11:49:13 +0200

    Added Movie getRenterPoints test

--- tests/MovieTest.cpp
+++ tests/MovieTest.cpp
@@ -18,3 +18,15 @@ TEST(Movie, getPrice)
     Movie movie3("The Joker", new MovieStateRelease);
     EXPECT_EQ(movie3.getPrice(10), 30);
 }
+
+TEST(Movie, getRenterPoints)
+{
+    Movie movie("The Incredibles", new MovieStateChildren);
+    EXPECT_EQ(movie.getRenterPoints(10), 0);
+
+    Movie movie2("The Goonies", new MovieStateRegular);
+    EXPECT_EQ(movie2.getRenterPoints(10), 0);
+
+    Movie movie3("The Joker", new MovieStateRelease);
+    EXPECT_EQ(movie3.getRenterPoints(10), 1);
+}
