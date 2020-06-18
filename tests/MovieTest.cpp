acfc0f68d4f0ef1409d57e48cb8f20d3a839b7a9
Robin <r.demarta@outlook.com>
2020-06-18 12:19:43 +0200

    Added mock for Movie tests

--- tests/MovieTest.cpp
+++ tests/MovieTest.cpp
@@ -7,26 +7,41 @@
 
 #include "../Movie.hpp"
 
+class MockMovieStateChildren : public MovieStateChildren {
+    MOCK_METHOD(double, getPrice, (int daysRented), (const));
+    MOCK_METHOD(int, getRenterPoints, (int daysRented), (const));
+};
+
+class MockMovieStateRegular : public MovieStateRegular {
+    MOCK_METHOD(double, getPrice, (int daysRented), (const));
+    MOCK_METHOD(int, getRenterPoints, (int daysRented), (const));
+};
+
+class MockMovieStateRelease : public MovieStateRelease {
+    MOCK_METHOD(double, getPrice, (int daysRented), (const));
+    MOCK_METHOD(int, getRenterPoints, (int daysRented), (const));
+};
+
 TEST(Movie, getPrice)
 {
-    Movie movie("The Incredibles", new MovieStateChildren);
+    Movie movie("The Incredibles", new MockMovieStateChildren);
     EXPECT_EQ(movie.getPrice(10), 12);
 
-    Movie movie2("The Goonies", new MovieStateRegular);
+    Movie movie2("The Goonies", new MockMovieStateRegular);
     EXPECT_EQ(movie2.getPrice(10), 14);
 
-    Movie movie3("The Joker", new MovieStateRelease);
+    Movie movie3("The Joker", new MockMovieStateRelease);
     EXPECT_EQ(movie3.getPrice(10), 30);
 }
 
 TEST(Movie, getRenterPoints)
 {
-    Movie movie("The Incredibles", new MovieStateChildren);
+    Movie movie("The Incredibles", new MockMovieStateChildren);
     EXPECT_EQ(movie.getRenterPoints(10), 0);
 
-    Movie movie2("The Goonies", new MovieStateRegular);
+    Movie movie2("The Goonies", new MockMovieStateRegular);
     EXPECT_EQ(movie2.getRenterPoints(10), 0);
 
-    Movie movie3("The Joker", new MovieStateRelease);
+    Movie movie3("The Joker", new MockMovieStateRelease);
     EXPECT_EQ(movie3.getRenterPoints(10), 1);
 }
