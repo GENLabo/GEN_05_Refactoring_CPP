a7308209f0f9d489cb34463e34e0445b140458ed
Robin <r.demarta@outlook.com>
2020-06-18 10:35:34 +0200

    WIP bugged tests

--- MovieStateRegular.hpp
+++ MovieStateRegular.hpp
@@ -4,8 +4,11 @@
 
 #ifndef GEN_CPP_REFACTORING_MOVIESTATEREGULAR_HPP
 #define GEN_CPP_REFACTORING_MOVIESTATEREGULAR_HPP
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 #include "MovieState.hpp"
+#include "Movie.hpp"
 
 class MovieStateRegular : public MovieState {
 public:
@@ -21,5 +24,15 @@ public:
         return 0;
     }
 };
+
+TEST(MovieStateRegular, getPrice) {
+    Movie movie("Avatar", new MovieStateRegular);
+    EXPECT_EQ(movie.getPrice(10), 12);
+}
+
+TEST(MovieStateRegular, getRenterPoints) {
+    Movie movie("Avatar 2", new MovieStateRegular);
+    EXPECT_EQ(movie.getRenterPoints(50), 0);
+}
 
 #endif //GEN_CPP_REFACTORING_MOVIESTATEREGULAR_HPP
